202246d0083ee183c16ce136a3176ab0a6ef049c
Alan Moore <kahunamoore@coopsource.org>
2016-02-21 15:33:28 -0800

    Added schema, license and datomic

--- src/cljs/koop/core.cljs
+++ src/cljs/koop/core.cljs
@@ -5,19 +5,37 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:name "Hello Chestnut!" :address "blah"}))
 
 (defui MemberView
 
-       static om/IQuery
-       (query [this]
-              [:name :address])
+  static om/Ident
+  (ident [this {:keys [id]}]
+    [:id id])
 
-       Object
-       (render [this] (dom/div #js {:className "member"} "Member: " )))
+  static om/IQuery
+  (query [this]
+    [:id :name :address])
+
+  Object
+  (render [this]
+    (dom/li #js {:className "member"} (:name (om/props this)) )))
 
 (def member-view (om/factory MemberView))
 
+(defui MembersView
+
+  static om/IQuery
+  (query [this]
+    [{:members (om/get-query MemberView)}])
+
+  Object
+  (render [this] (let [{:keys [members]} (om/props this)]
+                             (dom/ul #js {:className "members-list"}
+                                      (apply dom/div #js {:id "member-wrap"}
+                                             (map member-view members))))))
+
+
+(def members-view (om/factory MembersView))
 
 (defui HeaderView
        static om/IQuery
@@ -34,13 +52,14 @@
 (defui MainView
   static om/IQuery
   (query [this]
-    [:name :token])
+    [{:members (om/get-query MemberView)}])
 
   Object
-  (render [this]
-    (dom/div #js {:className "main"}
-                (member-view)
-                )))
+  (render [this] (let [{:keys [members]} (om/props this)]
+                   (dom/div #js {:className "main"}
+                            (js/alert members)
+                            ;(members-view data)
+                            ))))
 
 (def main-view (om/factory MainView))
 
@@ -68,10 +87,42 @@
 
 (def root-view (om/factory RootView))
 
+
+;;
+;; Data
+;;
+
+(defonce app-state (atom
+                     {:members  [{:id 123 :name "Alan" :address "11916 Big Blue Road"} {:id 456 :name "Dave" :address "123 Abbywood"}]
+                      :projects [{:id 976 :name "Hermes" :members [123 456]} {:id 654 :name "Clara" :members [123]}]}))
+
+;;
+;; Data read multimethods
+;;
+
+(defmulti read om/dispatch)
+
+(defn get-members [state key]
+  (let [st @state]
+    (into [] (map #(get-in st %)) (get st key))))
+
+(defn get-projects [state key]
+  (let [st @state]
+    (into [] (map #(get-in st %)) (get st key))))
+
+(defmethod read :members
+  [{:keys [state] :as env} key params]
+  {:value (get-members state key)})
+
+(defmethod read :projects
+  [{:keys [state] :as env} key params]
+  {:value (get-projects state key)})
+
 (def reconciler
   (om/reconciler
     {:state     (atom app-state)
      :normalize false
+     :read read
      ;:parser    (om/parser {:read p/read :mutate p/mutate})
      ;:send      (util/transit-post "/api")
      }))
